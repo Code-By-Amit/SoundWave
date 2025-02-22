2d4becf70e9969cd045f06a2b7bb7b6a40ccccf8
Code-By-Amit <amitsaini95471@gmail.com>
2025-02-22 18:22:17 +0530

    updated backend config for database

--- backend/config/connectToDB.js
+++ backend/config/connectToDB.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 async function connectToDB() {
-    mongoose.connect("mongodb://localhost:27017/SoundWave").then(()=>{
+    mongoose.connect(process.env.MongoDB_URL).then(()=>{
         console.log("Connected to mongoDb Successfully");
     }).catch((err)=>{
         console.log(`Failed to connect to mongoDb!! , Error :${err}`)
